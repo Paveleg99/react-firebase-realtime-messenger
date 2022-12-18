4cc853922d4fdcb5574e30271e227c0c52de1477
Paveleg99 <paveleg99@mail.ru>
2022-12-18 15:40:55 +0300

    Create navbar using Material UI

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import classes from './Navbar.module.css'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import { Button, Grid } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
 
 const Navbar = () => {
-  return (
-	<div> 
-	Navbar
-	</div>
-  )
+	const isAuth = false;
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar color='success' position="static">
+				<Toolbar>
+					<Grid container justifyContent="flex-end">
+						{isAuth
+							? <Button className={classes.loginBtn} variant="outlined" color='success'>Выйти</Button>
+							: <NavLink to='/login'>
+								<Button className={classes.loginBtn} variant="outlined" color='success'>Логин</Button>
+							</NavLink>
+						}
+					</Grid>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	)
 }
 
 export default Navbar;
